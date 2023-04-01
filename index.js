2c86ccdc53838ea6694511b16501412c0d1e716f
MrFabiioo <fabiolin57@gmail.com>
2023-04-01 01:58:15 -0500

    cors by autoritations of origin-req

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const routerApi = require('./routes');
 const express= require('express');
 const {errorHandler,logErros,boomErrorHandler} = require('./middlewares/error.handler');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+const whitelist = ["http://localhost:5500", "https:// myapp.com"];
+const options = {
+  origin:(origin,callback)=>{
+    if (whitelist.includes(origin)) {
+      callback(null,true);
+    } else {
+      callback(new Error('ACCESO NO PERMITIDO !!'));
+    }
+  }
+}
+app.use(cors(options));
 
 // ------- HOME  --------//
 app.get('/',(req,res)=>{
